55bb57dcffe909529ec6c1b922c8b0dde45f1b49
mars650 <bradleyd3@lyalnorm.com>
2024-10-20 02:31:47 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -16,6 +16,17 @@ export default async function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <h1>Some Text</h1>
+        <div>
+          {posts.map((post: any) => {
+              return (
+                <div key={post.name}>
+                <p>{post.name}</p>
+                {/* {JSON.stringify(post)} */}
+                </div>
+        
+              )
+            })}
+            </div>
 
         </main>
     </div>
